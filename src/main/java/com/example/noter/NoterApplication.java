a6600d8e7f8d8ae02d1a63de15831e6c66fd9781
swper <abc10130218@gmail.con>
2024-10-15 13:19:50 +0900

    first commit

---
+++ src/main/java/com/example/noter/NoterApplication.java
@@ -0,0 +1,13 @@
+package com.example.noter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noter/NoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
